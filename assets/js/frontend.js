48a1b2884b72e37110148af8a07bd64bc970de9f
Anthony Miller <1anthony1@gmail.com>
2018-09-22 12:56:59 -0700

    input mask

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,3 +1,4 @@
+/*-----------------Hide table-------------- */
 var submit = $("#confirm_push");
 var messageList = $(".card-body");
 
@@ -12,12 +13,51 @@ function hideList(){
         messageList.fadeIn("slow");
        
     })
-  
+/*-----------------End Hide table-------------- */
 
-    // $(submit).on('click', function() {
-    //     messageList.fadeIn("slow");
-    //     $(this).addClass('animated hinge').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-    //       $(this).removeClass('animated hinge');
-    //   });
-    // })
-  
+/*----------------- Input Mask-------------- */
+    $("#phone").inputmask({"mask": "(999) 999-9999"});
+ /*-------------End Input Mask--------------*/   
+
+/*----------------- Form Validation-------------- */
+
+    $(function () {
+        // Rules for submisson fields
+        $("#main_form").validate({
+            errorClass: "errors",
+            rules: {
+                name: {
+                    required: true
+                },
+                phone: {
+                    required: true,
+                    phone: true
+                },
+                occasion: {
+                    required: true,
+                },
+                date:
+                {
+                    required: true,
+                },
+                message: {
+                    required: true,
+                }
+            },
+            // Setting error messages for the fields
+            messages: {
+                name: "Please enter a name",
+                phone: "Please enter a valid phone number",
+                occasion: "Please enter an occasion",
+                message: "Please enter a message"
+            }
+        });
+
+
+    });
+
+    /*-------------End form Validation--------------*/
+
+
+
+
